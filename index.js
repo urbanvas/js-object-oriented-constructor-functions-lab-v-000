ad4b7746dd507ac6739947a0691fadfd2caf1923
Neil Seejoor <nvseejoor@gmail.com>
2018-10-07 22:36:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function Scooter(year, color, model) {
   this.year = year
-  this.color = color,
-  this.model = mode
+  this.color = color
+  this.model = model
 }
 function Driver(name, age, experience) {
   this.name = name
